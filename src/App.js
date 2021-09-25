8bf6c8a12476cfd86b7735472f7181a5abb70a5c
Clarice Bouwer <cbillowes@gmail.com>
2021-09-25 16:22:31 +0400

    create signup page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Header from './Header';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Signup from './pages/Signup';
 import './App.css';
 import './firebase/config';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header></Header>
       <div className="app">
-        <div className="ui grid container"></div>
+        <div className="ui grid container">
+          <Switch>
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
