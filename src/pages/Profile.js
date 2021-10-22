48c10b2dd6c7b07fb6b8d402d5ed6e0bcc041098
Clarice Bouwer <cbillowes@gmail.com>
2021-10-23 03:39:05 +0400

    display user document

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSession } from '../firebase/UserProvider';
+import { firestore } from '../firebase/config';
 
 const Profile = () => {
   const { user } = useSession();
+  const [ userDocument, setUserDocument ] = useState(null);
 
-  if (!user) {
+  useEffect(() => {
+    const docRef = firestore.collection('users').doc(user.uid);
+    const unsubscribe = docRef.onSnapshot((doc) => {
+      if (doc.exists) {
+        const documentData = doc.data();
+        setUserDocument(documentData);
+      }
+    })
+    return unsubscribe;
+  }, [user.uid]);
+
+  if (!userDocument) {
     return null;
   }
 
