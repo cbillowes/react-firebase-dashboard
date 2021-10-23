a5255f624214659498025c0afffc60e471df9e40
Clarice Bouwer <cbillowes@gmail.com>
2021-10-23 04:44:36 +0400

    trigger file upload

---
+++ src/ProfileImage.js
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+
+export const ProfileImage = ({ id }) => {
+  const fileInput = useRef(null);
+  const fileChange = (files) => {
+    console.log(files);
+  }
+
+  return (
+    <div className="four wide column profile-image">
+      <img
+        className="ui image"
+        src="/profile-placeholder.png"
+        alt="profile"
+      />
+      <input
+        className="file-input"
+        type="file"
+        accept=".png,.jpg"
+        ref={fileInput}
+        onChange={(e) => fileChange(e.target.files)}
+      />
+      <button
+        className="ui grey button upload-button"
+        onClick={() => fileInput.current.click()}
+      >
+        Upload photo
+      </button>
+    </div>
+  )
+}
